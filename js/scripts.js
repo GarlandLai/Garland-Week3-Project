e59fcd38e46d2bc38bf97558200fd0f5a756e378
Garland Lai <garlandlai24@gmail.com>
2019-06-14 10:35:02 -0700

    adds the functional for loop to get correct range

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    var numberInput = $("#userInput").val();
+    var numberInput = parseInt($("#userInput").val());
     // var outputAnswer = toReturn(numberInput)
     // $("#output").text(outputAnswer)
-    var resultNumbers = ["0"];
-    for (var index = 0; index < numberInput.length; index +=1){
-      resultNumbers.push(numberInput[index]);
+    var resultNumbers = [];
+    for (var index = 0; index <= numberInput; index +=1){
+      resultNumbers.push(index);
     };
-    resultNumbers = resultNumbers.map(function(resultNumber){
-      for (var index = 0; index <= resultNumbers.length; index +=1){
-        if (resultNumber[0] === 0) {
-          return resultNumber
-        }
-      }
-      console.log(numberInput);
+    // resultNumbers = resultNumbers.map(function(resultNumber){
+    //   for (var index = 0; index <= resu; index +=1){
+    //     resultNumbers += index
+    //      if (resultNumber[0] === 0) {
+    //       return resultNumber
+
+      // for (var index = 0; index <= resultNumbers.length; index +=1){
+      //   if (resultNumber[0] === 0) {
+      //     return resultNumber
+      //   }
+      // });
+      // console.log(numberInput);
       console.log(resultNumbers);
     });
   });
-});
