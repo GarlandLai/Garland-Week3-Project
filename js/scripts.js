49ebda7a6e0c244461fd1cfec3b5eda6ba28cb6d
Garland Lai <garlandlai24@gmail.com>
2019-06-14 13:15:21 -0700

    finally print out the output. had some issues finding out what was wrong.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,37 @@
+//user interface logic
 
 $(document).ready(function(){
   $("#formOne").submit(function(event){
     event.preventDefault();
-    var numberInput = parseInt($("#userInput").val());
+    var numberInput = $("#userInput").val();
     var resultNumbers = [];
-
-    for (var index = 0; index <= numberInput; index +=1){
+      for (var index = 0; index <= numberInput; index +=1){
       resultNumbers.push(index);
-    };
+      };
 
-    var resultStrings = resultNumbers.join('');
+      var resultStrings = resultNumbers.join('')
 
-    console.log(resultNumbers);
-    console.log(resultStrings);
-    console.log(resultStrings.length)
+      console.log(resultNumbers);
+      console.log(resultStrings);
+      console.log(resultStrings.length)
 
-    for (var index = 0; index <= resultStrings.length; index +=1) {
-      if (index === "1") {
-        return "Beep!"
-      } else if (index === "2"){
-        return "Boop!"
-      }
-        else if (index === index){
-        return index
-      }
-      $("#output").text(resultNumbers);
-    }
+
+//Business interface logic
+    // resultString = resultStrings.map(function(resultString){
+    // function toString
+    // for (var index = 0; index <= resultStrings.length; index +=1) {
+    //   if (index === "1") {
+    //     return "Beep!"
+    //   } else if (index === "2"){
+    //       return "Boop!"
+    //     }
+    //     else if (index === index){
+    //       return index
+    //   }
+    // };
+  var finished = resultNumbers.join('');
+  $("#output").text(resultNumbers);
   });
 
   // var rangeResult = resultStrings
 });
-
-// });
