67627a5893472de755331b59f0119aa723c25312
Garland Lai <garlandlai24@gmail.com>
2019-06-14 16:00:12 -0700

    some minor changes. saving before i attempt to make target multiple digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -5,35 +5,32 @@ $(document).ready(function(){
     event.preventDefault();
     var numberInput = $("#userInput").val();
     var resultNumbers = [];
-      for (var index = 0; index <= numberInput; index +=1){
+    for (var index = 0; index <= numberInput; index +=1){
       resultNumbers.push(index);
-    // var resultStrings = resultNumbers.join('')
     };
     console.log(resultNumbers);
     console.log(resultNumbers.length);
 
-    // console.log(resultStrings);
-    // console.log(resultStrings.length)
+    var specialNumbers = [1, 2 , 3]
 
-
-//Business interface logic
+    //Business interface logic
 
     resultNumbers = resultNumbers.map(function(num) {
       for (var index = 0; index <= resultNumbers.length; index +=1) {
-
-        if (num === 3) {
+        if (num === 1){
+          return "test"
+        }
+          else if (num === 3) {
           return " I'm sorry, Dave. I'm afraid I can't do that."
         } else if (num === 2){
-            return " Boop!"
+          return " Boop!"
         } else if (num === 1) {
-            return " Beep!"
+          return " Beep!"
         } else if (num === index){
-            return num
+          return num
         }
-  };
-});
-$("#output h3").text(resultNumbers);
-
-  // var rangeResult = resultStrings
-});
+      };
+    });
+    $("#output h3").text(resultNumbers);
+  });
 });
