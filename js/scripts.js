104e6a299edacac6a7a6d851b9b3f19ad7620201
Garland Lai <garlandlai24@gmail.com>
2019-06-14 13:47:33 -0700

    will change logic to get the length of each number, not specific digits

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,30 @@ $(document).ready(function(){
     var resultNumbers = [];
       for (var index = 0; index <= numberInput; index +=1){
       resultNumbers.push(index);
-      };
+    // var resultStrings = resultNumbers.join('')
+    };
+    console.log(resultNumbers);
+    console.log(resultNumbers.length);
 
-      var resultStrings = resultNumbers.join('')
-
-      console.log(resultNumbers);
-      console.log(resultStrings);
-      console.log(resultStrings.length)
+    // console.log(resultStrings);
+    // console.log(resultStrings.length)
 
 
 //Business interface logic
-    // resultString = resultStrings.map(function(resultString){
-    // function toString
-    // for (var index = 0; index <= resultStrings.length; index +=1) {
-    //   if (index === "1") {
-    //     return "Beep!"
-    //   } else if (index === "2"){
-    //       return "Boop!"
-    //     }
-    //     else if (index === index){
-    //       return index
-    //   }
-    // };
-  var finished = resultNumbers.join('');
-  $("#output").text(resultNumbers);
-  });
+
+    resultNumbers = resultNumbers.map(function(str) {
+      for (var index = 0; index <= resultNumbers.length; index +=1) {
+        if (index === 1) {
+          return "Beep!"
+        } else if (index === 2){
+            return "Boop!"
+        } else if (index === index){
+            return index
+        }
+  };
+});
+$("#output").text(resultNumbers);
 
   // var rangeResult = resultStrings
+});
 });
